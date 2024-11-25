62c4410a7ea17f2fe62ad33a5fdd2f28761d6189
Anupam Shankar <anupam.shankar24@gmail.com>
2024-11-25 13:26:12 +0000

    added start over button

--- src/pages/ThankYouPage.js
+++ src/pages/ThankYouPage.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { replace, useNavigate } from "react-router-dom";
 
 const ThankYouPage = () => {
+  const navigate = useNavigate();
+
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Thank You!</h1>
-      <p>You can now close this window.</p>
+      <h1>Form Submitted. Thanks!</h1>
+      <button
+        style={{
+          backgroundColor: "#28a745",
+          border: "none",
+          color: "white",
+          padding: "10px 20px",
+          cursor: "pointer",
+          borderRadius: "4px",
+          fontSize: "14px",
+          fontWeight: "bold",
+        }}
+        onClick={() => navigate("/nasa-tlx", { replace: true })}
+      >
+        Start Over
+      </button>
     </div>
   );
 };
